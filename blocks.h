c206732266590e0366c1ce9a7d4ce2c4f3b59cfc
Spyros Roum <spyrosr1@gmail.com>
2021-01-15 13:54:40 +0200

    Use pulse instead of amixer

--- blocks.h
+++ blocks.h
@@ -1,19 +1,15 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "cat ~/.pacupdate | sed /📦0/d",					0,		9},
-	
-	{"🧠", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+    {" ", "~/.local/bin/dwm_spotify", 5, 11},
 
-	{"", "~/bin/statusbar/volume",						0,		10},
+	{"🧠 ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	20,		0},
 
-	{"☀", "xbacklight | sed 's/\\..*//'",					0,		11},
-	
-	{"", "~/bin/statusbar/battery",						5,		0},
+	{"", "~/.local/bin/dwm_pulse",					        	0,		10},
 
-	{"🌡", "sensors | awk '/^temp1:/{print $2}'",				5,		0},
+    {"🌡 ", "sensors | awk '/^temp1:/{print $2}'",			  20,       0},
 
-	{"", "~/bin/statusbar/clock",						5,		0},
+	{"📆 ", "date \"+%a, %b %d %Y, %H:%M%p\"",				   20,		0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
